a76d4ce17d5135602408f5341d86242d7902c65b
Gowtham <gowtham.vasista@gmail.com>
2021-07-17 20:49:30 +0530

    Create Black Scholes.py

---
+++ Black Scholes.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jul 17 20:07:21 2021
+
+@author: Gowtham
+"""
+
+import math
+from scipy.stats import norm
+
+class option:
+    
+    def __int__(self, optionType, underlyingPrice, strike, rate, underlyingVol, timeToExpiry):
+        
+        self.optionType = optionType
+        self.underlyingPrice = underlyingPrice
+        self.strike - strike
+        self.rate = rate
+        self.underlyingVol = underlyingVol
+        self.timeToExpiry = timeToExpiry
+        
+        if(self.)
+        
+    def callOption():
+        print("Call Option")
+        
+    def putOption():
+        print("put option")
