1ebd793d3d6048c42da6fb55e844cd586f5de13a
Gowtham <gowtham.vasista@gmail.com>
2021-07-18 10:52:20 +0530

    Update Black Scholes.py

--- Black Scholes.py
+++ Black Scholes.py
@@ -21,41 +21,51 @@ class Option:
         self.underlyingVol = underlyingVol
         self.timeToExpiry = timeToExpiry
         
-        # Call price calculation methods as per option type
-        if(optionType == "call"):
-            self.price = self.callOption()
-        elif(optionType == "put"):
-            self.price = self.putOption(underlyingPrice, strike, rate, underlyingVol, timeToExpiry)
+        
+        # # Call price calculation methods as per option type
+        # if(optionType == "call"):
+        #     self.price = self.callPrice()
+        # elif(optionType == "put"):
+        #     self.price = self.putPrice()
+        # else:
+        #     print("Invalid Option Type")
+        
+        
+        
+        
+    def Price(self):
+        
+        if(self.optionType == "call"):
+            
+            # Compute d1
+            d1 = (math.log(self.underlyingPrice / self.strike) + (((self.rate + pow(self.underlyingVol, 2))/2) * self.timeToExpiry)) / (self.underlyingVol * math.sqrt(self.timeToExpiry)) 
+            
+            # Compute d2
+            d2 = d1 - (self.underlyingVol * math.sqrt(self.timeToExpiry))
+            
+            # Lookup N(d1)
+            nd1 = norm.cdf(d1)
+            
+            # Lookup N(d2)
+            nd2 = norm.cdf(d2)
+            
+            # Calc call option price
+            price = (self.underlyingPrice * nd1) - (self.strike * math.exp(-self.rate *  self.timeToExpiry) * nd2)
+            
+            return price
+        
+        elif(self.optionType == "Put"):
+            
+            return 0
+        
         else:
-            print("Invalid Option Type")
+            return False
         
-        
-        
-        
-    def callOption(self):
-        
-        # Compute d1
-        d1 = (math.log(self.underlyingPrice / self.strike) + (((self.rate + pow(self.underlyingVol, 2))/2) * self.timeToExpiry)) / (self.underlyingVol * math.sqrt(self.timeToExpiry)) 
-        
-        # Compute d2
-        d2 = d1 - (self.underlyingVol * math.sqrt(self.timeToExpiry))
-        
-        # Lookup N(d1)
-        nd1 = norm.cdf(d1)
-        
-        # Lookup N(d2)
-        nd2 = norm.cdf(d2)
-        
-        # Calc call option price
-        price = (self.underlyingPrice * nd1) - (self.strike * math.exp(-self.rate *  self.timeToExpiry) * nd2)
-        
-        return price
-        
-    
     
     def putOption():
         print("put option")
         
         
 option = Option("call", 125.94, 125, 0.0446, 0.83, 0.0959)
-print(option.price)
+price = option.Price()
+print(price)
