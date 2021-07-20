24595459d9b96c31e16800e2df1e511919d961d1
Gowtham <gowtham.vasista@gmail.com>
2021-07-20 23:58:11 +0530

    Update put_call_parity.py

--- put_call_parity.py
+++ put_call_parity.py
@@ -11,9 +11,26 @@ sys.path.append(".")
 
 from option import Option
 
+# Option(Type, spot, strike,rate, vol, time to maturity)
+put_option = Option("put", 125.94, 125, 0.0446, 0.83, 0.0959)
+call_option= Option("call", 125.94, 125, 0.0446, 0.83, 0.0959)
 
 
+put_premium = put_option.price()
+call_premium = call_option.price()
+strike = put_option.option_strike
+spot = put_option.underlying_price
+riskfree_rate = put_option.riskfree_rate
+time_to_maturity = put_option.time_to_expiry
 
-option = Option("put", 125.94, 125, 0.0446, 0.83, 0.0959)
-price = option.price()
-print(price)
+# LHS of put call parity
+lhs = put_premium + spot
+
+# RHS of put call parity
+rhs = call_premium + (strike / pow((1 + riskfree_rate), time_to_maturity))
+
+## check if LHS and RHS are equal
+if(round(lhs,1) == round(rhs, 1)):
+    print("Equal")
+else:
+    print("Not equal")
